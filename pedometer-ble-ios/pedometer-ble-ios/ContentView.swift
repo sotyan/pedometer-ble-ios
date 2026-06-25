7d0eae446b85b51561cf7fee35231c1ba283fd22
Sota Tsukada <113979340+sotyan@users.noreply.github.com>
2026-06-25 13:55:26 +0900

    親機・子機モードの選択画面の実装と遷移先の作成

---
+++ pedometer-ble-ios/pedometer-ble-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pedometer-ble-ios
+//
+//  Created by k24098kk on 2026/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pedometer-ble-ios/pedometer-ble-ios/pedometer_ble_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pedometer_ble_iosApp.swift
+//  pedometer-ble-ios
+//
+//  Created by k24098kk on 2026/06/24.
+//
+
+import SwiftUI
+
+@main
+struct pedometer_ble_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
